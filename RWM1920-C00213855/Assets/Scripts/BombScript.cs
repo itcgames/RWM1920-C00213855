a15143c2fbd8cad3107f91d8034af8d8d66a8296
patrykwysocki <patrykwysocki>
2019-11-11 21:16:29 +0000

    bomb feature started

---
+++ RWM1920-C00213855/Assets/Scripts/BombScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour
+{
+    bool m_exploded = false;
+    public float m_delay = 1.0f;
+    public float m_explosion_rate = 5.0f;
+    public float m_maxSize = 100.0f;
+    public float m_explosionSpeed = 10.0f;
+    public float m_currentRadius = 0.0f;
+
+    CircleCollider2D m_explosion_radius;
+
+    private void Start()
+    {
+        m_explosion_radius = gameObject.GetComponent<CircleCollider2D>();
+    }
+
+    private void Update()
+    {
+        m_delay -= Time.deltaTime;
+        if(m_delay < 0)
+        {
+            m_exploded = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(m_exploded==true)
+        {
+            if(m_currentRadius < m_maxSize)
+            {
+                m_currentRadius += m_explosion_rate;
+            }
+            else
+            {
+                Object.Destroy(this.gameObject.transform.parent.gameObject);
+            }
+
+            m_explosion_radius.radius = m_currentRadius;
+        }
+       
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(m_exploded==true)
+        {
+            if(col.gameObject.GetComponent<Rigidbody2D>() != null)
+            {
+                Vector2 targetPos = col.gameObject.transform.position;
+                Vector2 bombPos = gameObject.transform.position;
+                Vector2 blastDir = 100.0f * (targetPos - bombPos);
+                col.gameObject.GetComponent<Rigidbody2D>().AddForce(blastDir);
+
+            }
+
+        }
+    }
+}
