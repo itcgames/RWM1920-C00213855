208459e655606ebeee30ad8e43ef4d55edad7ff5
patrykwysocki <patrykwysocki>
2020-01-26 13:57:11 +0000

    Feature 3 Animations complete

--- RWM1920-C00213855/Assets/Scripts/CollisionDetection.cs
+++ RWM1920-C00213855/Assets/Scripts/CollisionDetection.cs
@@ -6,14 +6,23 @@ public class CollisionDetection : MonoBehaviour
 {
     public BombScript Bomb;
     public GameObject pp;
+    private Vector3 originalPos;
+    private void Start()
+    {
+        originalPos = pp.GetComponentInChildren<Transform>().position;
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.transform.gameObject.tag == "PressurePlate")
         {
+
             Debug.Log("Player collided with Pressure Plate");
             Bomb.SetActive(true);
             pp.GetComponent<AudioSource>().Play();
             Bomb.GetComponentInParent<Animator>().SetBool("Test", true);
+            pp.GetComponentInChildren<Transform>().position -= new Vector3(0, 0.15f, 0);
         }
+
+
     }
 }
